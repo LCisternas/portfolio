1ff7326b97fce9a98ef62ccf0d27633c2b03b1c2
LCisternas <l.cisternasopazo@gmail.com>
2025-09-05 23:29:38 -0400

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Lucas Cisternas',
   description: 'Lucas Cisternas, software engineer',
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({
